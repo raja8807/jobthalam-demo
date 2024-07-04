55e931711c1219689a15ff438ea45c7ab1d132b3
Raja <raja.rathinam@pcisage.com>
2024-07-04 22:52:03 +0530

    service change

--- src/pages/api/auth/getToken.js
+++ src/pages/api/auth/getToken.js
@@ -1,17 +1,28 @@
 import admin from "firebase-admin";
 import { getApps, getApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import x from './jt-test-26ac5-9db62a1858fb.json'
+import service from "./service.json";
 
 const handler = async (req, res) => {
 
+  service.type = process.env.NEXT_PUBLIC_TYPE;
+  service.project_id = process.env.NEXT_PUBLIC_PROJECT_ID;
+  service.private_key_id = process.env.NEXT_PUBLIC_PRIVATE_KEY_ID;
+  service.private_key = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+  service.client_email = process.env.NEXT_PUBLIC_CLIENT_EMAIL;
+  service.client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
+  service.auth_uri = process.env.NEXT_PUBLIC_AUTH_URI;
+  service.token_uri = process.env.NEXT_PUBLIC_TOKEN_URI;
+  service.auth_provider_x509_cert_url =
+  process.env.NEXT_PUBLIC_AUTH_PROVIDER__CERT_URL;
+  service.client_x509_cert_url = process.env.NEXT_PUBLIC_CLIENT_CERT_URL;
+  service.universe_domain = process.env.NEXT_PUBLIC_UNIVERSAL_DOMAIN;
 
   try {
-    // console.log(x);
     const firebaseAdmin = !getApps().length
       ? admin.initializeApp({
           credential: admin.credential.cert(
-            x
+            service
           ),
         })
       : getApp();
@@ -19,7 +30,7 @@ const handler = async (req, res) => {
     const token = await getAuth(firebaseAdmin).createCustomToken(
       req?.body?.mobile,
       {
-        role:'candidate'
+        role: "candidate",
       }
     );
 
