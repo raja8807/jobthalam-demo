411dffc838c9e7a3cefe95e9070bd92bc1046a03
Raja <raja.rathinam@pcisage.com>
2024-07-06 22:36:46 +0530

    job card ui

--- src/components/screens/admin/tabs/joo_requests/request_details/send_jobs/send_jobs.jsx
+++ src/components/screens/admin/tabs/joo_requests/request_details/send_jobs/send_jobs.jsx
@@ -7,6 +7,7 @@ import { ArrowLeft, Trash, X } from "react-bootstrap-icons";
 import styles from "./send_jobs.module.scss";
 import JobTable from "@/components/jobs/job_table/job_table";
 import { addData, addMultipleData, updateData } from "@/libs/firebase/firebase";
+import JobCard from "@/components/ui/job/job_card/job_card";
 
 const SendJobs = ({
   setShow,
@@ -42,7 +43,7 @@ const SendJobs = ({
 
       if (updateRes) {
         setShow((prev) => ({ ...prev, jobs_sent: allSent.length }));
-        setNewJobs([])
+        setNewJobs([]);
       }
     } catch (error) {
       console.log(error);
@@ -87,25 +88,27 @@ const SendJobs = ({
         title="Jobs To Send"
         count={`${newJobs.length}/${request.count - request.jobs_sent}`}
       >
-        {newJobs.map((job) => {
-          return (
-            <CustomTableRow key={job.id}>
-              <Col>{job.title}</Col>
-              <Col>DataCom</Col>
-              <Col>
-                <CustomButton variant={2}>Details</CustomButton>
-                <X
-                  className={styles.x}
-                  onClick={() => {
-                    setNewJobs((prev) => {
-                      return prev.filter((nj) => nj.id != job.id);
-                    });
-                  }}
-                />
-              </Col>
-            </CustomTableRow>
-          );
-        })}
+        <Row>
+          {newJobs.map((job) => {
+            return (
+              <JobCard
+                key={job.id}
+                job={job}
+                actionButton={
+                  <CustomButton
+                    onClick={() => {
+                      setNewJobs((prev) => {
+                        return prev.filter((nj) => nj.id != job.id);
+                      });
+                    }}
+                  >
+                    Remove
+                  </CustomButton>
+                }
+              />
+            );
+          })}
+        </Row>
       </CustomTable>
       <br />
       {newJobs.length > 0 && (
@@ -128,27 +131,42 @@ const SendJobs = ({
           })
           .map((job) => {
             return (
-              <Col key={job.id} xs={12} md={4} lg={3} className={styles.job}>
-                <div>{job.title}</div>
-                <div>Employer &nbsp;: Data Com</div>
-                <div>Experience : {job.experience}</div>
-                <br />
-                <div>
-                  <CustomButton variant={2}>Details</CustomButton>
-                  &nbsp;
-                  <CustomButton
-                    onClick={() => {
-                      setNewJobs((prev) => [job, ...prev]);
-                    }}
-                    disabled={
-                      request.count - request.jobs_sent <= newJobs.length
-                    }
-                  >
-                    Add
-                  </CustomButton>
-                </div>
-                {/* <div></div> */}
-              </Col>
+              <JobCard
+                key={job.id}
+                job={job}
+                actionButton={
+                  !(request.count - request.jobs_sent <= newJobs.length) && (
+                    <CustomButton
+                      onClick={() => {
+                        setNewJobs((prev) => [job, ...prev]);
+                      }}
+                    >
+                      Add
+                    </CustomButton>
+                  )
+                }
+              />
+              // <Col key={job.id} xs={12} md={4} lg={3} className={styles.job}>
+              //   <div>{job.title}</div>
+              //   <div>Employer &nbsp;: Data Com</div>
+              //   <div>Experience : {job.experience}</div>
+              //   <br />
+              //   <div>
+              //     <CustomButton variant={2}>Details</CustomButton>
+              //     &nbsp;
+              //     <CustomButton
+              //       onClick={() => {
+              //         setNewJobs((prev) => [job, ...prev]);
+              //       }}
+              //       disabled={
+              //         request.count - request.jobs_sent <= newJobs.length
+              //       }
+              //     >
+              //       Add
+              //     </CustomButton>
+              //   </div>
+              //   {/* <div></div> */}
+              // </Col>
             );
           })}
       </Row>
