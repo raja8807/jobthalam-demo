baa00a250bd179d740b7702b070b43a4a1f844fa
Raja <raja.rathinam@pcisage.com>
2024-07-08 00:23:52 +0530

    #header chnages

--- src/components/layout/header/main_header/main_header.jsx
+++ src/components/layout/header/main_header/main_header.jsx
@@ -8,37 +8,37 @@ import { auth } from "@/libs/firebase/firebase";
 import CustomDropDown from "@/components/ui/dropdown/dropdown";
 import { CaretDown, CaretDownFill } from "react-bootstrap-icons";
 
-const LoginDropDown = ({setShowLogin}) => {
-  return (
-    <div className={styles.LoginDropDown}>
-      <CustomDropDown
-        button={
-          <div className={styles.login_button}>
-            <p>
-              Login <CaretDownFill />
-            </p>
-          </div>
-        }
-        options={[
-          {
-            id: "xx",
-            title: "Login as Employer",
-            href:"https://jobthalam-employer.vercel.app/",
-            target:"_blank"
-          },
-          {
-            id: "aa",
-            title: "Login as Candidate",
-            variant: "primary",
-            onClick: () => {
-              setShowLogin(true);
-            },
-          },
-        ]}
-      />
-    </div>
-  );
-};
+// const LoginDropDown = ({setShowLogin}) => {
+//   return (
+//     <div className={styles.LoginDropDown}>
+//       <CustomDropDown
+//         button={
+//           <div className={styles.login_button}>
+//             <p>
+//               Login <CaretDownFill />
+//             </p>
+//           </div>
+//         }
+//         options={[
+//           {
+//             id: "xx",
+//             title: "Login as Employer",
+//             href:"https://jobthalam-employer.vercel.app/",
+//             target:"_blank"
+//           },
+//           {
+//             id: "aa",
+//             title: "Login as Candidate",
+//             variant: "primary",
+//             onClick: () => {
+//               setShowLogin(true);
+//             },
+//           },
+//         ]}
+//       />
+//     </div>
+//   );
+// };
 
 const MainHeader = ({ currentUser, session }) => {
   const [showLogin, setShowLogin] = useState(false);
@@ -71,7 +71,6 @@ const MainHeader = ({ currentUser, session }) => {
                   </CustomButton>
                 </div>
 
-                <LoginDropDown setShowLogin={setShowLogin}/>
               </>
             )}
             {session && !currentUser && (
--- src/components/layout/header/top_header/top_header.jsx
+++ src/components/layout/header/top_header/top_header.jsx
@@ -1,45 +1,69 @@
 import CustomContainer from "@/components/ui/custom_container/custom_container";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./tom_header.module.scss";
 import PAGES from "@/constants/pages";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { List } from "react-bootstrap-icons";
+import HeaderDrawer from "./header_drawer/header_drawer";
 
 const TopHeader = ({ session }) => {
   const router = useRouter();
 
+  const [showHeader, setShowHeader] = useState(false);
+
   return (
     <nav className={styles.top_nav}>
       <CustomContainer>
-        <ul>
-          {PAGES.map((p) => {
-            if (p.isPortal) {
-              if (session) {
+        <div className={styles.wrap}>
+          <div className={styles.left}>
+            <div href="/" className={styles.logo}></div>
+          </div>
+
+          <ul>
+            {PAGES.map((p) => {
+              if (p.isPortal) {
+                if (session) {
+                  return (
+                    <li
+                      key={"portal"}
+                      className={`${styles.item} ${
+                        router.pathname === p.href ? styles.active : ""
+                      }`}
+                    >
+                      <Link href={"/candidate"}>Candidate</Link>
+                    </li>
+                  );
+                }
+              } else {
                 return (
                   <li
-                    key={'portal'}
+                    key={p.name}
                     className={`${styles.item} ${
                       router.pathname === p.href ? styles.active : ""
                     }`}
                   >
-                    <Link href={"/candidate"}>Candidate</Link>
+                    <Link href={p.href}>{p.name}</Link>
                   </li>
                 );
               }
-            } else {
-              return (
-                <li
-                  key={p.name}
-                  className={`${styles.item} ${
-                    router.pathname === p.href ? styles.active : ""
-                  }`}
-                >
-                  <Link href={p.href}>{p.name}</Link>
-                </li>
-              );
-            }
-          })}
-        </ul>
+            })}
+          </ul>
+
+          <HeaderDrawer
+            show={showHeader}
+            router={router}
+            setShow={setShowHeader}
+            session={session}
+          />
+
+          <List
+            className={styles.menu}
+            onClick={() => {
+              setShowHeader(true);
+            }}
+          />
+        </div>
       </CustomContainer>
     </nav>
   );
