3fa3124e8703f5f327163db5692b50b93a14f6fd
Raja <raja.rathinam@pcisage.com>
2024-07-30 14:18:29 +0530

    pg

--- src/libs/sequelize/sequelize.ts
+++ src/libs/sequelize/sequelize.ts
@@ -1,10 +1,12 @@
 // lib/sequelize.ts
 import { Sequelize } from "sequelize";
+import * as pg from "pg";
 
 const sequelize = new Sequelize(
   "postgres://default:CIsr1Xk0lcGU@ep-mute-thunder-a7gws162.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require" as string,
   {
     dialect: "postgres",
+    dialectModule: pg,
   }
 );
 
--- src/pages/api/application/index.js
+++ src/pages/api/application/index.js
@@ -1,10 +1,8 @@
 import Application from "@/libs/sequelize/Models/Application";
-import sequelize from "@/libs/sequelize/sequelize";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
     try {
-      await sequelize.sync();
       const result = await Application.create(req.body, {
         returning: true,
       });
