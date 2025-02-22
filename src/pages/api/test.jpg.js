d81d674ddd79e552f24415e4576136167ae98e3e
Raja <raja.rathinam@pcisage.com>
2025-02-22 18:13:13 +0530

    test

--- src/pages/api/test.jpg.js
+++ src/pages/api/test.jpg.js
@@ -1,19 +1,19 @@
+import { addData } from "@/libs/firebase/firebase";
 import { fsync } from "fs";
 import path from "path";
+import { v4 } from "uuid";
 // import fs from "fs";
 
 const handler = async (req, res) => {
   try {
     console.log("Tracking Image Loaded:");
+    console.log(req.cookies);
 
-  // Path to an actual image file in your project (public folder)
-  const imagePath = path.join(process.cwd(), "public", "user.jpg");
+    var id = v4();
+    await addData("test", { ...req.cookies, id }, id);
 
-  // Read and send the image file
-  const imageBuffer = fsync.readFileSync("imagePath");
-  res.setHeader("Content-Type", "image/jpeg");
-  res.send(imageBuffer);
-    
+    const imagePath = path.join(__dirname, "user.jpg"); // Use any image
+    res.sendFile(imagePath);
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({
